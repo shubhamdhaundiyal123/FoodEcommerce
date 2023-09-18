807f412a4c9314782bbdced9f6e232ccfa3aba9a
Shubham <shubhamdhaundiyal789@gmail.com>
2023-09-18 11:38:03 +0530

    Navbar Successfully Created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import {BsSearch} from 'react-icons/bs'
+import {AiOutlineUser,AiOutlineShoppingCart} from 'react-icons/ai'
+import CartCountBadge from './CartCountBadge'
+
+const Navbar = () => {
+  return (
+    <div className="container hidden lg:block" >
+      <div className="flex justify-between items-center pt-8">
+        <h1 className="text-4xl font-medium ">SD Store</h1>
+        <div className="relative w-full max-w-[500px]">
+            <input className="bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full" type="text" placeholder="Search Product..." />
+            <BsSearch className="absolute right-0 top-0 mt-4 mr-5 text-gray-500" size={20} />
+        </div>
+        <div className="flex gap-4">
+            <div className="icon__wrapper">
+                <AiOutlineUser/>
+            </div>
+            <div className="icon__wrapper relative">
+                <AiOutlineShoppingCart/>
+                <CartCountBadge size="w-[25px] h-[25px]" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
